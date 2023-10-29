2b5dbb84e735462acbfe2d8f838c0ff5aef1ca30
ChiakiIzutsu <chiaki.izutsu@Chiaki-MacBook-Pro13-inchM12020.local>
2023-10-29 15:28:42 +0900

    optionAPI basic configuration

---
+++ src/views/BasicConfiguration.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+<script>
+  const app = Vue.createApp ({
+    data () {
+      return {
+        // アプリケーションが保持するデータをここに定義する
+        message: 'Hello Vue.js!'
+      }
+    },
+    methods: {
+      // アプリケーションが持つメソッドをここで定義する
+    },
+    computed: {
+      // アプリケーションが持つ算出プロパティをここで定義する
+    },
+    watch: {
+      // アプリケーションが持つウォッチャをここで定義する
+    }
+  })
+const vm = app.mount ('#app');
+</script>
+<style></style>
